766e767deb7d8c05bbe9599f82516405b3b193fc
DooHee Choi <doing84@naver.com>
2016-03-02 20:10:31 -0500

    midterm

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public int tankCount;
+	public GameObject tank;
+	private int _scoreValue;
+	private int _livesValue;
+
+	public int ScoreValue {
+		get {
+			return this._scoreValue;
+		}
+		
+		set {
+			this._scoreValue = value;
+
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		this._GenerateTanks ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// generate Clouds
+	private void _GenerateTanks() {
+		for (int count=0; count < this.tankCount; count++) {
+			Instantiate(tank);
+		}
+	}
+}
